4e632c0de0068a40dca4c7833050c111b9059696
AntonSemenov87 <Anthony04061987@gmail.com>
2020-12-12 12:13:44 -0600

    updates

---
+++ src/test/java/api_tests/HR_API_Tests_With_Parameters.java
@@ -0,0 +1,7 @@
+package api_tests;
+
+public class HR_API_Tests_With_Parameters {
+
+
+
+}
--- src/test/java/api_tests/SpartanTestsWithParameters.java
+++ src/test/java/api_tests/SpartanTestsWithParameters.java
@@ -5,6 +5,9 @@ import io.restassured.response.Response;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.Assert.*;
 
@@ -18,7 +21,7 @@ public class SpartanTestsWithParameters {
     @Test
     public void helloTest(){
         // request
-        Response response = get("64");
+        Response response = get("/64");
         // response
         // verify status code 200
         System.out.println(response.getStatusLine());
@@ -65,5 +68,53 @@ public class SpartanTestsWithParameters {
         assertTrue(response.body().asString().contains("Female"));
 
     }
+
+    /*
+    Given accept type is XML
+    And query parameter values are:
+    gender | Female
+    nameContains | e
+    When user sends GET request to api/spartans/search
+    Then response status should be 406
+    And "Could not find acceptable representation" should be in response payload
+     */
+
+
+
+    @Test
+    public void invalid_header_test_with_parameters () {
+
+        Response response1 = given().accept(ContentType.XML)
+                            .queryParams("gender", "Female", "nameContains", "e")
+                            .when().get("/spartans/search");
+
+        Response response2 = given().accept(ContentType.XML)
+                .queryParams("gender", "Female")
+                .queryParams("nameContains", "e")
+                .when().get("/spartans/search");
+
+        Map<String, Object> paramsMap = new HashMap<>();
+        paramsMap.put("gender", "Female");
+        paramsMap.put("nameContains", "e");
+
+        Response response3 = given().accept(ContentType.XML)
+                .queryParams(paramsMap)
+                .when().get("/spartans/search");
+
+
+        // Response validations with JUnit assertions
+        assertEquals(406, response1.statusCode());
+        assertEquals(406, response2.statusCode());
+        assertEquals(406, response3.statusCode());
+
+        assertTrue(response1.body().asString().contains("Could not find acceptable representation"));
+        assertTrue(response2.body().asString().contains("Could not find acceptable representation"));
+        assertTrue(response3.body().asString().contains("Could not find acceptable representation"));
+
+
+
+
+    }
+
 
 }
